dd01d06cdf7f6361a99e306ff9ab5cce30dd0b97
Santhosh-Rangaraj <santhoshvav@gmail.com>
2026-08-19 22:55:17 +0530

    feat: enhance Project page layout and add responsive sidebar styles

--- project-hub/src/pages/Project.tsx
+++ project-hub/src/pages/Project.tsx
@@ -1,7 +1,18 @@
+import { Plus } from 'lucide-react';
 
+import"../styles/Project.css";
 const Project=()=>{
     return(
+        <>
+        <div className='project-header'>
+        <div className='project-header-text'>
         <h1>Project</h1>
+        <p>Manage and track all your projects</p>
+        </div>
+        <button><Plus /> Add Project</button>
+        </div>
+        </>
+        
     )
 }
 
